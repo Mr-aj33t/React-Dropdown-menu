95f444db0e61c2ea5a43ca9e7f0f53432383548b
Ajeet Kumar <93984178+devil-69@users.noreply.github.com>
2024-01-07 03:40:32 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ export default function App() {
   const [selected, setSelected] = useState("");
   return (
     <div className="App">
-      {/* custom dropdown menu */}
       <Dropdown selected={selected} setSelected={setSelected} />
     </div>
   );
